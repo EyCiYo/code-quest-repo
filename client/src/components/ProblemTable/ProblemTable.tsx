a6162aa0293028c4e60afeb15ec173ce0e20e07e
ARJ18 <arjunb1802@gmail.com>
2024-03-27 18:11:29 +0530

    problemtable component modified

--- client/src/components/ProblemTable/ProblemTable.tsx
+++ client/src/components/ProblemTable/ProblemTable.tsx
@@ -6,81 +6,108 @@ import { FaFlagCheckered } from "react-icons/fa";
 import { AiFillYoutube } from "react-icons/ai";
 import { IoClose } from "react-icons/io5";
 import YouTube from "react-youtube";
-
+import Topbar from "@/components/Topbar/Topbar";
 type ProblemTableProps = {};
 
 const ProblemTable: React.FC<ProblemTableProps> = () => {
-    const [youtubePlayer, setYoutubePlayer] = useState({
-        isOpen: false,
-        videoId: "",
+  const [youtubePlayer, setYoutubePlayer] = useState({
+    isOpen: false,
+    videoId: "",
+  });
+
+  const closeModal = () => {
+    setYoutubePlayer({
+      isOpen: false,
+      videoId: "",
     });
-
-    const closeModal = () => {
-        setYoutubePlayer({
-            isOpen: false,
-            videoId: "",
-        });
+  };
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key == "Escape") closeModal();
     };
-    useEffect(() => {
-        const handleEscape = (e: KeyboardEvent) => {
-            if (e.key == "Escape") closeModal();
-        };
 
-        window.addEventListener("keydown", handleEscape);
-    }, []);
-    return (
-        <>
-            <tbody className="text-white ">
-                {problems.map((doc, idx) => {
-                    const difficultyColor =
-                        doc.difficulty == "Easy"
-                            ? "text-dark-green-s"
-                            : doc.difficulty == "Medium"
-                            ? "text-dark-yellow"
-                            : "text-dark-pink";
-                    return (
-                        <tr
-                            className={`${
-                                idx % 2 == 1 ? "bg-dark-layer-1" : ""
-                            }`}
-                            key={doc.id}
-                        >
-                            <th className="px-2 py-4 font-medium text-dark-green-s whitespace-nowrap">
-                                <FaFlagCheckered fontSize={"18"} width="18" />
-                            </th>
-                            <td className="px-6 py-4">
-                                <Link
-                                    className="hover:text-blue-400 cursor-pointer"
-                                    href={`/problems/${doc.id}`}
-                                >
-                                    {doc.title}
-                                </Link>
-                            </td>
-                            <td className={`px-6 py-4 ${difficultyColor}`}>
-                                {doc.difficulty}
-                            </td>
-                            <td className={"px-6 py-4"}>{doc.category}</td>
-                            <td className="px-6 py-4">
-                                {doc.videoId ? (
-                                    <AiFillYoutube
-                                        fontSize={"28"}
-                                        className="cursor-pointer hover:text-red-600"
-                                        onClick={() =>
-                                            setYoutubePlayer({
-                                                isOpen: true,
-                                                videoId: doc.videoId as string,
-                                            })
-                                        }
-                                    />
-                                ) : (
-                                    <p className="text-gray-400">Coming soon</p>
-                                )}
-                            </td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-            {youtubePlayer.isOpen && (
+    window.addEventListener("keydown", handleEscape);
+  }, []);
+  return (
+    <>
+      <Topbar clock={false} />
+      <h1 className="text-2xl text-center text-gray-700 dark:text-gray-400 font-medium uppercase mt-10 mb-5">
+        LOOK AT THESE QUESTIONS
+      </h1>
+
+      <div className="relative overflow-x-auto mx-auto px-6 pb-10">
+        <table className="text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto">
+          <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 border-b ">
+            <tr>
+              <th scope="col" className="px-1 py-3 w-0 font-medium">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3 w-0 font-medium">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3 w-0 font-medium">
+                Difficulty
+              </th>
+
+              <th scope="col" className="px-6 py-3 w-0 font-medium">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3 w-0 font-medium">
+                Solution
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-white ">
+            {problems.map((doc, idx) => {
+              const difficultyColor =
+                doc.difficulty == "Easy"
+                  ? "text-dark-green-s"
+                  : doc.difficulty == "Medium"
+                  ? "text-dark-yellow"
+                  : "text-dark-pink";
+              return (
+                <tr
+                  className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
+                  key={doc.id}
+                >
+                  <th className="px-2 py-4 font-medium text-dark-green-s whitespace-nowrap">
+                    <FaFlagCheckered fontSize={"18"} width="18" />
+                  </th>
+                  <td className="px-6 py-4">
+                    <Link
+                      className="hover:text-blue-400 cursor-pointer"
+                      href={`/problems/${doc.id}`}
+                    >
+                      {doc.title}
+                    </Link>
+                  </td>
+                  <td className={`px-6 py-4 ${difficultyColor}`}>
+                    {doc.difficulty}
+                  </td>
+                  <td className={"px-6 py-4"}>{doc.category}</td>
+                  <td className="px-6 py-4">
+                    {doc.videoId ? (
+                      <AiFillYoutube
+                        fontSize={"28"}
+                        className="cursor-pointer hover:text-red-600"
+                        onClick={() =>
+                          setYoutubePlayer({
+                            isOpen: true,
+                            videoId: doc.videoId as string,
+                          })
+                        }
+                      />
+                    ) : (
+                      <p className="text-gray-400">Coming soon</p>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      {/* {youtubePlayer.isOpen && (
                 <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center ">
                     <div
                         className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"
@@ -103,8 +130,8 @@ const ProblemTable: React.FC<ProblemTableProps> = () => {
                         </div>
                     </div>
                 </tfoot>
-            )}
-        </>
-    );
+            )} */}
+    </>
+  );
 };
 export default ProblemTable;
